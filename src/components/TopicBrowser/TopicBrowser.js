e1dd77e095540fe97123472a1126b4e53754c24e
david <davidwnewman91@gmail.com>
2019-05-13 16:45:13 -0500

    Done

--- src/components/TopicBrowser/TopicBrowser.js
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
+import EvenAndOdd from '../Topics/EvenAndOdd';
+import FilterObject from '../Topics/FilterObject';
+import filterString from '../Topics/FilterString';
+import Palindrome from '../Topics/Palindrome';
+import Sum from '../Topics/Sum';
+import FilterString from '../Topics/FilterString';
 
 class TopicBrowser extends Component{
     render(){
         return(
-            <p>Hello World</p>
+            <div>
+                <EvenAndOdd />
+                <FilterObject />
+                <FilterString />
+                <Palindrome />
+                <Sum />
+            </div>
         )
     }
 }
--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,38 @@
 import React, {Component} from 'react';
 
 class Palindrome extends Component{
+    constructor(){
+        super()
+        this.state = {
+            userInput: '',
+            palindrome: ''
+        }
+    }
+
+    handleChange(v){
+        this.setState({userInput: v});
+    }
+    palindrome(userInput){
+        let prepped = userInput.split('');
+        let reversed = prepped.reverse();
+        let restrung = reversed.join('');
+        
+        if(restrung === userInput){
+            this.setState({palindrome: 'true'});
+        }
+        else{
+            this.setState({palindrome: 'false'})
+        }
+    }
+
     render(){
         return(
-            <p>Palindrome Component</p>
+            <div className='puzzleBox palindromePB'>
+                <h4>Palindrome</h4>
+                <input className='inputLine' onChange={(e)=>this.handleChange(e.target.value)}></input>
+                <button className='confirmationButton' onClick={()=>this.palindrome(this.state.userInput)}>Check</button>
+                <span className='resultsBox'>Palindrome: {this.state.palindrome}</span>
+            </div>
         )
     }
 }
